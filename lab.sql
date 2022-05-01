ce579450757fbd2fd788e77dd0a7f3bf7fb0e7cd
adina1998 <97709520+adina1998@users.noreply.github.com>
2022-04-30 23:51:31 -0400

    commit

--- lab.sql
+++ lab.sql
@@ -1,13 +1,99 @@
-CREATE DATABASE `horror_quiz`;
-USE `horror_quiz`;
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: May 01, 2022 at 03:47 AM
+-- Server version: 5.7.34
+-- PHP Version: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `horror_quiz`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answers`
+--
+
+CREATE TABLE `answers` (
+  `id` int(255) NOT NULL,
+  `answer1` varchar(255) NOT NULL,
+  `answer2` varchar(255) NOT NULL,
+  `answer3` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `answers`
+--
+
+INSERT INTO `answers` (`id`, `answer1`, `answer2`, `answer3`) VALUES
+(1, 'anthonyhopkins', 'skeetulrich', 'tonicollete');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
 
 CREATE TABLE `questions` (
-  `id` int(11) AUTO_INCREMENT,
-  `question` varchar(255),
-  `question2` varchar(255),
-  `question3` varchar(255),
-  PRIMARY KEY(`id`)
-);
+  `id` int(255) NOT NULL,
+  `question1` varchar(255) NOT NULL,
+  `question2` varchar(255) NOT NULL,
+  `question3` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `horror_quiz` (`id`, `question`, `question2`, `question3`) VALUES
-(1,`value`, `value2`, `value3`);
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`id`, `question1`, `question2`, `question3`) VALUES
+(1, ' Who is the male lead actor in the movie above?', 'Who was the original Ghost face?', 'Who played the mother in the film above?');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `answers`
+--
+ALTER TABLE `answers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `answers`
+--
+ALTER TABLE `answers`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
