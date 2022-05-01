e3a70bf7207521d8c54472688aa704538b922890
adina1998 <97709520+adina1998@users.noreply.github.com>
2022-04-30 23:41:14 -0400

    commit

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ try{
         if(mysqli_num_rows($result)){
             while($rows = mysqli_fetch_array($result))
         {
-            echo($rows['answer']);
+            echo($rows['answers']);
         }
     }
         }else{
