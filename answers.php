559f48329065bf2c233653d03847bfb6d4089866
adina1998 <97709520+adina1998@users.noreply.github.com>
2022-04-30 22:30:47 -0400

    commit

--- answers.php
+++ answers.php
@@ -1,25 +1,49 @@
-<!-- <?php
-include("data.php");
-
-function data_answerNextQuestion() {
-    // Validate and sanitize.
-    $result = data_sanitize();
-    // Open connection.
-    data_connect();
-
-    // Use connection.
-    //
-    // We want to make sure we don't add
-    //  duplicate values.
-    if(!data_verifyAnswer($result["questions"], $result["answers"])) {
-        // Username does not exist.
-        // Add a new one.
-        data_answerNextQuestion($result["questions"], $result["answers"]);
+<?php
+if(isset($_POST['submit'])){
+    $anthonyhopkins=$_POST['anthonyhopkins'];
+    $query= "INSERT INTO `questions`(`questions`) 
+    VALUES (false, 'anthonyhopkins')";
+    try {
+        $result = mysqli_query($connection, $query);
+        if($result){
+            if(mysqli_affected_rows($connection)>0){
+                echo "data submitted";
+            }
+            else {
+                echo "data not submitted";
+            }
+        }
+    } catch(Exception $ex){
+        echo ("error in connection");
     }
-    
-    // Close connection.
-    data_close();
-}
+    $querytwo = "select question.question, answers.answer from answers inner join
+    (select question from questions order by id desc limit 1)
+    as question on question.question = answers.answer";
+    $resulttwo = mysqli_query($connection, $querytwo);
+    if($resulttwo){
+        if(mysqli_affected_rows($connection)>0){
+            $querythree="INSERT INTO `compare`(`value`) VALUES ('correct')";
+            $resultthree=mysqli_query($connection,$querythree);
+        }else{
+                $querythree = "INSERT INTO `compare`(`value`) VALUES ('incorrect')";
+                $resultthree=mysqli_query($connection, $querythree);
 
+            }
+    }
+ if (isset($_POST['Check'])){
+     $queryfour = "select value from compare order by id desc limit 1";
+     $resultfour = mysqli_query($connection, $queryfour);
+     if($resultfour){
+         if(mysqli_num_rows($resultfour)){
+             while($rows = mysqli_fetch_array($resultfour))
+             {
+                 echo($rows['value']."<br>");
+             }
+         }
+     }else{
+         echo "error in result";
+     }
+ }   
+}}
 
-?> -->
+?>
