c2f6c3e66adb5d7ef435f3bd69c09660d8dca6ac
adina1998 <97709520+adina1998@users.noreply.github.com>
2022-04-30 20:27:29 -0400

    commit

--- data.php
+++ data.php
@@ -15,49 +15,61 @@ function data_connect() {
     }
 }
 
-function data_answerNextQuestion($questions, $answers) {
-    // Use the global connection
-    global $connection;
-
-    if($connection != null) {
-        // Overwrite the existing password value as a hash
-        $questions = questions_hash($questions, );
-        // Insert username and hashed password
-        mysqli_query($connection, "INSERT INTO `questions`(`questions`)
-        VALUES ('answers')
-       ");
+    try{
+        $connection = mysqli_connect($server, $username, $password, $database);
     }
-}
-
-
-function data_verifyAnswer($questions, $answers) {
-    // Use the global connection
-    global $connection;
-
-    // Create a default value
-    $status = false;
-
-    if($connection != null) {
-        // Use WHERE expressions to look for username
-        $results = mysqli_query($connection, "INSERT INTO `questions`(`questions`)
-         VALUES ('answers')
-        ");
-        
-        // mysqli_fetch_assoc() returns either null or row data
-        $row = mysqli_fetch_assoc($results);
-        
-        // If $row is not null, it found row data.
-        if($row != null) {
-            // Verify password against saved hash
-            if(question_verifyAnswer($questions, $row["results"])) {
-                $status = true;
-            }}
+        catch(MySQLi_Sql_Exception $ex)
+        {
+            echo("error");
         }
+    }
+if(isset($_POST['submit'])){
+    $thesilenceofthelambs=$_POST['thesilenceofthelambs'];
+    $query="INSERT INTO `questions`(`questions`) 
+    VALUES ('thesilenceofthelambs')";
+    try {
+        $result = mysqli_query($connection, $query);
+        if($result){
+            if(mysqli_affected_rows($connection)>0){
+                echo "data submitted";
+            }
+            else {
+                echo "data not submitted";
+            }
+        }
+    } catch(Exception $ex){
+        echo ("error in connection");
+    }
+    $querytwo = "select question.question, answers.answer from answers inner join
+    (select question from questions order by id desc limit 1)
+    as question on question.question = answers.answer";
+    $resulttwo = mysqli_query($connection, $querytwo);
+    if($resulttwo){
+        if(mysqli_affected_rows($connection)>0){
+            $querythree="INSERT INTO `compare`(`value`) VALUES ('correct')";
+            $resultthree=mysqli_query($connection,$querythree);
+        }else{
+                $querythree = "INSERT INTO `compare`(`value`) VALUES ('incorrect')";
+                $resultthree=mysqli_query($connection, $querythree);
 
-
-    return $status;
+            }
+    }
+ if (isset($_POST['Check'])){
+     $queryfour = "select value from compare order by id desc limit 1";
+     $resultfour = mysqli_query($connection, $queryfour);
+     if($resultfour){
+         if(mysqli_num_rows($resultfour)){
+             while($rows = mysqli_fetch_array($resultfour, MYSQL_BOTH))
+             {
+                 echo($rows['value']."<br>");
+             }
+         }
+     }else{
+         echo "error in result";
+     }
+ }   
 }
-
+}
 function data_close() {
     global $connection;
     
